f8fc91b77072c5a2ded359f81c1dcccecb0bbb28
Jie Zhou <jzhou4894@eagle.fgcu.edu>
2021-09-30 03:19:44 -0400

    add todo and todoitem

---
+++ Client/TodoItem.cs
@@ -0,0 +1,5 @@
+public class TodoItem                     // declare a public class, name it Todoitem
+{
+    public string Title { get; set; }      //function that return string
+    public bool IsDone { get; set; }        // function that return bool value
+}
